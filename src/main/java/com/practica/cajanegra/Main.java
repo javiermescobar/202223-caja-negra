8390b04a0050e2cd1c374029c81a67921858a791
Javier Martín Escobar <javier.mescobar@alumnos.upm.es>
2023-06-11 21:35:28 +0200

    Create main test and java classes

---
+++ src/main/java/com/practica/cajanegra/Main.java
@@ -0,0 +1,11 @@
+package com.practica.cajanegra;
+
+import com.binarytree.BinaryTree;
+
+public class Main {
+
+    private static BinaryTree testTree;
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/practica/cajanegra/TestMain.java
@@ -0,0 +1,10 @@
+package com.practica.cajanegra;
+
+import com.binarytree.BinaryTree;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestMain {
+
+}
